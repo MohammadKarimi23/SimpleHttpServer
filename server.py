181776a8840c2f831bffb83c1cea03f50848f001
Moolica <mohammadkarimi.ceit@gmail.com>
2017-04-03 19:09:26 +0430

    idk

--- server.py
+++ server.py
@@ -1,31 +1,57 @@
 #!/usr/bin/python
 
 import socket
-import sys
+import sys , getopt
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('0.0.0.0', 10000)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
-sock.bind(server_address)
+#def main(argv):
+#    port = ''
+#    try: 
+#        opts, args = getopt(argv,"hp:",["port="])
+#    except getopt.GetoptError:
+#        print 'server.py -p <portnumber>'
+#        sys.exit(2)
+#    for opt, arg in opts:
+#        if opt == '-h':
+#            print 'server.py -p <portNumber>'
+#            sys.exit()
+#        elif opt in ("-p","--port"):
+#            port = arg
+#    print 'port numm is "', port
+#     server(int(port))
+    
 
-sock.listen(1)
 
-while True:
-    print >>sys.stderr, 'waiting for a connection'
-    connection, client_address = sock.accept()
-    try:
-        print >>sys.stderr, 'connection from', client_address
+def server(port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('0.0.0.0', port)
+    print >>sys.stderr, 'starting up on %s port %s' % server_address
+    sock.bind(server_address)
+    
+    sock.listen(1)
 
-        while True:
-            data = connection.recv(1024)
-            print >>sys.stderr, 'received'
-            print >>sys.stderr, '"%s"' % data
-            if data:
-                print >>sys.stderr, 'sending data back to the client'
+    while True:
+        print >>sys.stderr, 'waiting for a connection'
+        connection, client_address = sock.accept()
+        try:
+            print >>sys.stderr, 'connection from', client_address
+    
+            while True:
+                data = connection.recv(64)
+                print >>sys.stderr, 'received'
+                print >>sys.stderr, '"%s"' % data
+                if data:
+                    print >>sys.stderr, 'sending data back to the client'
 #               connection.sendall(data)
-            else:
-                print >>sys.stderr, 'no more data from', client_address
-                break
+                else:
+                    print >>sys.stderr, 'no more data from', client_address
+                    break
             
-    finally:
-        connection.close()
+        finally:
+            connection.close()
+
+def parseMessage(msg):
+    sleep(5)
+
+if __name__ == '__main__':
+#    main(sys.argv[1:])
+    server(1242)
